ba39eee738eace930f394953a82fe6381da2e1ca
nbouvier <48826816+nbouvier@users.noreply.github.com>
2022-07-31 15:02:34 +0200

    Delete config-local.ts

--- config/config-local.ts
+++
@@ -1,7 +0,0 @@
-export const Con: Record<string, string> = {
-  "OracleTest": "0x5FbDB2315678afecb367f032d93F642f64180aa3",
-  "OracleId": "0x01923b5b9da37231a909e5c9abfef015cb03a0ecdc101226142f625109715ba2",
-  "Lottery": "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
-  "RngOracleStorage": "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
-  "NFTContract": "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
-}
